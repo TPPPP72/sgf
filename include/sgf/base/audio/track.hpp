1965732a8de7607680b45b009802e10d9260aa15
TPPPP72 <906483498@qq.com>
2026-01-11 16:40:21 +0800

    feat(track): add APIs
    - add state enum class
    - add bind_audio(audio)
    - add play(bool is_loop)
    - add pause()
    - add resume()
    - add get_state()

--- include/sgf/base/audio/track.hpp
+++ include/sgf/base/audio/track.hpp
@@ -1,6 +1,7 @@
 #ifndef SGF_BASE_TRACK_HPP
 #define SGF_BASE_TRACK_HPP
 
+#include "audio.hpp"
 #include "mixer.hpp"
 
 namespace sgf::base
@@ -9,13 +10,41 @@ namespace sgf::base
 class track
 {
 public:
+    enum class state
+    {
+        wait_for_audio,
+        wait_for_play,
+        playing,
+        paused
+    };
+
+public:
     track(const mixer &);
     ~track();
     void *get() const noexcept;
+    void bind_audio(const audio &);
+    void play(bool is_loop);
+    bool is_playing() const noexcept;
+    // when is_playing == false, try play can work
+    bool try_play(bool is_loop);
+    void pause();
+    bool is_paused() const noexcept;
+    // when is_pause == false , try paused can work
+    bool try_pause();
+    void resume();
+    bool can_resume() const noexcept;
+    // when can_resume == true , try resume can work
+    bool try_resume();
+    state get_state() const noexcept;
+    // check if track has played finished
+    void update_state();
 
 private:
     struct impl;
     std::unique_ptr<impl> p_impl;
+    const audio *p_audio{nullptr};
+    bool p_is_loop{false};
+    state p_state{state::wait_for_audio};
 };
 
 } // namespace sgf::base
--- src/base/SDL_impl/track.cpp
+++ src/base/SDL_impl/track.cpp
@@ -1,3 +1,4 @@
+#include "SDL3/SDL_error.h"
 #include <SDL3_mixer/SDL_mixer.h>
 #include <sgf/base/audio/track.hpp>
 #include <stdexcept>
@@ -25,4 +26,108 @@ track::~track()
 void *track::get() const noexcept
 {
     return p_impl->track;
+}
+
+void track::bind_audio(const audio &aud)
+{
+    p_audio = &aud;
+
+    if (!MIX_SetTrackAudio(p_impl->track, static_cast<MIX_Audio *>(p_audio->get())))
+        throw std::runtime_error(SDL_GetError());
+
+    p_state = state::wait_for_play;
+}
+
+void track::play(bool is_loop)
+{
+    if (p_state != state::wait_for_play)
+        throw std::runtime_error("Can not play a track before bind_audio");
+
+    auto props{SDL_CreateProperties()};
+    SDL_SetNumberProperty(props, MIX_PROP_PLAY_LOOPS_NUMBER, is_loop ? -1 : 0);
+
+    if (!MIX_PlayTrack(p_impl->track, props))
+    {
+        SDL_DestroyProperties(props); // none raii object should be released when throw error
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    SDL_DestroyProperties(props);
+
+    p_state = state::playing;
+}
+
+bool track::is_playing() const noexcept
+{
+    return p_state == state::playing;
+}
+
+bool track::try_play(bool is_loop)
+{
+    if (is_playing())
+        return false;
+
+    play(is_loop);
+    return true;
+}
+
+void track::pause()
+{
+    if (p_state != state::playing)
+        throw std::runtime_error("Can not pause a track not playing");
+
+    if (!MIX_PauseTrack(p_impl->track))
+        throw std::runtime_error(SDL_GetError());
+
+    p_state = state::paused;
+}
+
+bool track::is_paused() const noexcept
+{
+    return p_state == state::paused;
+}
+
+bool track::try_pause()
+{
+    if (is_paused())
+        return false;
+
+    pause();
+    return true;
+}
+
+void track::resume()
+{
+    if (p_state != state::paused)
+        throw std::runtime_error("Can not resume a track not paused");
+
+    if (!MIX_ResumeTrack(p_impl->track))
+        throw std::runtime_error(SDL_GetError());
+
+    p_state = state::playing;
+}
+
+bool track::can_resume() const noexcept
+{
+    return p_state == state::paused;
+}
+
+bool track::try_resume()
+{
+    if (!can_resume())
+        return false;
+
+    resume();
+    return true;
+}
+
+track::state track::get_state() const noexcept
+{
+    return p_state;
+}
+
+void track::update_state()
+{
+    if (!MIX_TrackPlaying(p_impl->track) && p_audio != nullptr)
+        p_state = state::wait_for_play;
 }
