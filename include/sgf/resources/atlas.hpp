2d6e02128bcb66ecac4f1c92dd66188ba8de2140
TPPPP72 <906483498@qq.com>
2026-01-16 10:41:24 +0800

    feat(resources): add atlas and sprite resource definitions

---
+++ include/sgf/resources/atlas.hpp
@@ -0,0 +1,13 @@
+#ifndef SGF_RESOURCES_ATLAS_HPP
+#define SGF_RESOURCES_ATLAS_HPP
+
+namespace sgf::resources
+{
+
+class atlas
+{
+};
+
+} // namespace sgf::resources
+
+#endif
---
+++ include/sgf/resources/sprite.hpp
@@ -0,0 +1,13 @@
+#ifndef SGF_RESOURCES_SPRITE_HPP
+#define SGF_RESOURCES_SPRITE_HPP
+
+namespace sgf::resources
+{
+
+class sprite
+{
+};
+
+} // namespace sgf::resources
+
+#endif
