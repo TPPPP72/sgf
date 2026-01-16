5054189dc0073e2421d1c3062326f7083d313878
TPPPP72 <906483498@qq.com>
2026-01-16 10:41:37 +0800

    feat(animation): introduce animation clip and player types

---
+++ include/sgf/animation/clip.hpp
@@ -0,0 +1,13 @@
+#ifndef SGF_ANIMATION_CLIP_HPP
+#define SGF_ANIMATION_CLIP_HPP
+
+namespace sgf::animation
+{
+
+class clip
+{
+};
+
+} // namespace sgf::animation
+
+#endif
---
+++ include/sgf/animation/player.hpp
@@ -0,0 +1,13 @@
+#ifndef SGF_ANIMATION_PLAYER_HPP
+#define SGF_ANIMATION_PLAYER_HPP
+
+namespace sgf::animation
+{
+
+class player
+{
+};
+
+} // namespace sgf::animation
+
+#endif
