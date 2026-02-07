d72d8b9b4e5ffde597d2725cd9cc53f68f8f8512
TPPPP <906483498@qq.com>
2026-02-07 21:48:14 +0800

    feat(kernel): implement kernel

--- include/sgf/kernel.hpp
+++ include/sgf/kernel.hpp
@@ -2,7 +2,9 @@
 #define SGF_KERNEL_HPP
 
 #include "base/renderer.hpp"
+#include "base/viewport.hpp"
 #include "base/window/window.hpp"
+#include "sgf/input_system.hpp"
 #include "type/size.hpp"
 #include <chrono>
 #include <cstdint>
@@ -16,15 +18,24 @@ class kernel
 {
 public:
     // Create window with no VSync
-    kernel(const std::string &title, const type::world_size &ws, std::uint32_t max_fps) : p_ws(ws), p_window({title, ws.w, ws.h}), p_renderer(p_window)
+    kernel(const std::string &title, const type::world_size &ws, std::uint32_t max_fps) : p_ws(ws), p_max_fps(max_fps), p_window({title, ws.w, ws.h}), p_renderer(p_window), p_viewport(p_window)
     {
-        if (max_fps == 0)
-            p_renderer.set_vsync(true);
-        else
-            p_renderer.set_vsync(false);
+        init();
     }
     // Create window with VSync
     kernel(const std::string &title, const type::world_size &ws) : kernel(title, ws, 0) {}
+    base::window &get_window() noexcept
+    {
+        return p_window;
+    }
+    base::renderer &get_renderer() noexcept
+    {
+        return p_renderer;
+    }
+    std::uint32_t get_current_fps() const noexcept
+    {
+        return p_current_fps;
+    }
     // Launch game
     void run()
     {
@@ -38,9 +49,10 @@ public:
         ns accumulator{0};
         auto last_time = clock::now();
 
-        bool running = true;
+        auto fps_last_time = clock::now();
+        std::uint32_t frame_count{0};
 
-        while (running)
+        while (true)
         {
             auto now      = clock::now();
             ns frame_time = std::chrono::duration_cast<ns>(now - last_time);
@@ -49,7 +61,7 @@ public:
             frame_time = std::min(frame_time, max_frame_time);
             accumulator += frame_time;
 
-            running = control();
+            control();
 
             std::int32_t updates = 0;
             while (accumulator >= logic_step && updates < max_updates_per_frame)
@@ -63,6 +75,16 @@ public:
                 accumulator = ns{0};
 
             render();
+
+            ++frame_count;
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - fps_last_time);
+
+            if (duration.count() >= 500)
+            {
+                p_current_fps = static_cast<double>(frame_count) / (duration.count() / 1000.0);
+                frame_count   = 0;
+                fps_last_time = now;
+            }
 
             if (p_max_fps > 0)
             {
@@ -78,15 +100,44 @@ public:
     }
 
 private:
-    void control() {}
-    void update(std::chrono::nanoseconds dt) {}
-    void render() {}
+    void init()
+    {
+        if (p_max_fps == 0)
+            p_renderer.set_vsync(true);
+        else
+            p_renderer.set_vsync(false);
+
+        input_system::instance().bind(&p_window);
+        p_window.on_resize([this]()
+                           {
+                               p_viewport.update();
+                           });
+    }
+    void control()
+    {
+        p_window.poll_event();
+        input_system::instance().update();
+    }
+    void update(std::chrono::nanoseconds dt)
+    {
+        static_cast<game *>(this)->on_update(*this, dt);
+    }
+    void render()
+    {
+        p_renderer.begin_frame(p_viewport);
+        p_renderer.clear();
+        static_cast<game *>(this)->on_render(*this);
+        p_renderer.present();
+        p_renderer.end_frame();
+    }
 
 private:
     type::world_size p_ws{0, 0};
     std::uint32_t p_max_fps{0};
+    std::uint32_t p_current_fps{0};
     base::window p_window;
     base::renderer p_renderer;
+    base::viewport p_viewport;
 };
 
 } // namespace sgf
