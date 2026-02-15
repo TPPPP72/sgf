22791c095acbf10f1665c44c1169c3b66ddea939
TPPPP <906483498@qq.com>
2026-02-15 21:05:06 +0800

    feat(camera): implement camera

---
+++ include/sgf/camera/camera.hpp
@@ -0,0 +1,45 @@
+#ifndef SGF_CAMERA_HPP
+#define SGF_CAMERA_HPP
+
+#include "../type/position.hpp"
+#include "../type/rect.hpp"
+#include "../type/size.hpp"
+#include <algorithm>
+
+namespace sgf
+{
+
+class camera
+{
+public:
+    camera(const type::view_size vs, const type::world_size ws) : p_view_size(vs), p_world_size(ws), p_world_position(0, 0) {}
+    type::world_position position() const noexcept
+    {
+        return p_world_position;
+    }
+    void set_position(const type::world_position pos)
+    {
+        auto target_x = pos.x - p_view_size.w / 2.0;
+        auto target_y = pos.y - p_view_size.h / 2.0;
+
+        p_world_position.x = std::max(0.0, std::min(target_x, static_cast<double>(p_world_size.w - p_view_size.w)));
+        p_world_position.y = std::max(0.0, std::min(target_y, static_cast<double>(p_world_size.h - p_view_size.h)));
+    }
+    type::view_position to_view_pos(const type::world_position pos) const noexcept
+    {
+        return {pos.x - p_world_position.x, pos.y - p_world_position.y};
+    }
+    type::view_rect to_view_rect(const type::world_rect rect) const noexcept
+    {
+        return {rect.x - p_world_position.x, rect.y - p_world_position.y, rect.w, rect.h};
+    }
+
+private:
+    type::view_size p_view_size;
+    type::world_size p_world_size;
+    type::world_position p_world_position;
+};
+
+} // namespace sgf
+
+#endif
