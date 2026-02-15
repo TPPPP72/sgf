08ad0c9286d7a3524afa48d614c790b11d29d3eb
TPPPP <906483498@qq.com>
2026-02-15 17:12:40 +0800

    feat(game_object_pool): improve the safety of the game object pool

--- src/game_object/game_object_pool.cpp
+++ src/game_object/game_object_pool.cpp
@@ -16,18 +16,32 @@ sgf::game_object *sgf::game_object_pool::get(sgf::game_object_handle handle)
 
 sgf::game_object_handle sgf::game_object_pool::create(std::unique_ptr<sgf::game_object> obj)
 {
+    std::size_t target_idx;
+    std::uint32_t target_ver;
+    bool reused = false;
+
     for (std::size_t i = 0; i < p_objects.size(); ++i)
     {
         if (p_objects[i].obj == nullptr)
         {
-            ++p_objects[i].version;
             p_objects[i].obj = std::move(obj);
-            return {i, p_objects[i].version};
+            target_ver       = ++p_objects[i].version;
+            target_idx       = i;
+            reused           = true;
+            break;
         }
     }
 
-    p_objects.push_back({std::move(obj), 1});
-    return {p_objects.size() - 1, 1};
+    if (!reused)
+    {
+        p_objects.push_back({std::move(obj), 1});
+        target_idx = p_objects.size() - 1;
+        target_ver = 1;
+    }
+
+    p_objects[target_idx].obj->on_init();
+
+    return {target_idx, target_ver};
 }
 
 bool sgf::game_object_pool::remove(sgf::game_object_handle handle)
@@ -40,6 +54,7 @@ bool sgf::game_object_pool::remove(sgf::game_object_handle handle)
     if (entry.obj != nullptr && entry.version == handle.version)
     {
         entry.obj.reset();
+        ++entry.version;
         return true;
     }
 
@@ -50,16 +65,17 @@ void sgf::game_object_pool::update(std::chrono::nanoseconds dt)
 {
     for (std::size_t i = 0; i < p_objects.size(); ++i)
     {
-        auto &entry = p_objects[i];
+        if (p_objects[i].obj && p_objects[i].obj->is_active())
+            p_objects[i].obj->on_update(dt);
+    }
 
-        if (entry.obj == nullptr)
-            continue;
-
-        if (entry.obj->is_active())
-            entry.obj->on_update(dt);
-
-        if (entry.obj->is_pending_destruction())
-            entry.obj.reset();
+    for (std::size_t i = 0; i < p_objects.size(); ++i)
+    {
+        if (p_objects[i].obj && p_objects[i].obj->is_pending_destruction())
+        {
+            p_objects[i].obj.reset();
+            ++p_objects[i].version;
+        }
     }
 }
 
