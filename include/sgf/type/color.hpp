1f7725a885569765a057ed3be67a2a4f16227840
TPPPP <906483498@qq.com>
2026-02-17 14:11:40 +0800

    feat(type): import more kind of color types

--- include/sgf/type/color.hpp
+++ include/sgf/type/color.hpp
@@ -2,29 +2,85 @@
 #define SGF_TYPE_COLOR_HPP
 
 #include <cstdint>
+#include <limits>
+#include <type_traits>
 
 namespace sgf::type
 {
 
-struct color
+template <typename T>
+struct color_limits
 {
-    std::uint8_t r{0};
-    std::uint8_t g{0};
-    std::uint8_t b{0};
-    std::uint8_t a{0};
+    static constexpr T max()
+    {
+        if constexpr (std::is_floating_point_v<T>)
+            return static_cast<T>(1.0);
+        else
+            return std::numeric_limits<T>::max();
+    }
 
-    static const color red;
-    static const color green;
-    static const color blue;
-    static const color white;
-    static const color black;
+    static constexpr T min()
+    {
+        return static_cast<T>(0);
+    }
 };
 
-inline constexpr color color::red{255, 0, 0, 255};
-inline constexpr color color::green{0, 255, 0, 255};
-inline constexpr color color::blue{0, 0, 255, 255};
-inline constexpr color color::white{255, 255, 255, 255};
-inline constexpr color color::black{0, 0, 0, 255};
+template <typename T>
+struct color_t
+{
+    constexpr color_t() = default;
+    constexpr color_t(T r, T g, T b, T a)
+        : r(r), g(g), b(b), a(a) {}
+
+    T r{0};
+    T g{0};
+    T b{0};
+    T a{0};
+
+    static const color_t black;
+    static const color_t white;
+    static const color_t red;
+    static const color_t green;
+    static const color_t blue;
+
+    template <typename U>
+    constexpr color_t(const color_t<U> &other)
+        : r(convert_v<U, T>(other.r)),
+          g(convert_v<U, T>(other.g)),
+          b(convert_v<U, T>(other.b)),
+          a(convert_v<U, T>(other.a)) {}
+
+private:
+    template <typename From, typename To>
+    static constexpr To convert_v(From val)
+    {
+        if constexpr (std::is_same_v<From, To>)
+            return val;
+        if constexpr (std::is_floating_point_v<To> && std::is_integral_v<From>)
+            return static_cast<To>(val) / static_cast<To>(255);
+        if constexpr (std::is_integral_v<To> && std::is_floating_point_v<From>)
+            return static_cast<To>(val * 255);
+        return static_cast<To>(val);
+    }
+};
+
+template <typename T>
+inline constexpr color_t<T> color_t<T>::black{0, 0, 0, color_limits<T>::max()};
+
+template <typename T>
+inline constexpr color_t<T> color_t<T>::white{color_limits<T>::max(), color_limits<T>::max(), color_limits<T>::max(), color_limits<T>::max()};
+
+template <typename T>
+inline constexpr color_t<T> color_t<T>::red{color_limits<T>::max(), 0, 0, color_limits<T>::max()};
+
+template <typename T>
+inline constexpr color_t<T> color_t<T>::green{0, color_limits<T>::max(), 0, color_limits<T>::max()};
+
+template <typename T>
+inline constexpr color_t<T> color_t<T>::blue{0, 0, color_limits<T>::max(), color_limits<T>::max()};
+
+using color  = color_t<std::uint8_t>;
+using colorf = color_t<float>;
 
 } // namespace sgf::type
 
