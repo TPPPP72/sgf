c4dc9408d8c46a14fa76eb0019a09810200613c9
TPPPP <906483498@qq.com>
2026-02-18 19:18:42 +0800

    feat(game_object): add transform

--- include/sgf/game_object/game_object.hpp
+++ include/sgf/game_object/game_object.hpp
@@ -4,11 +4,21 @@
 #include "../base/renderer.hpp"
 #include "../type/position.hpp"
 #include "../type/size.hpp"
+#include "../type/vec2.hpp"
 #include <chrono>
 #include <cstdint>
 
 namespace sgf
 {
+
+struct transform
+{
+    type::world_position pos{0.0, 0.0};
+    type::world_size size{0, 0};
+    type::vec2f pivot{0.5f, 0.5f};
+    float rotation{0.0f};
+    float scale{1.0f};
+};
 
 class game_object
 {
@@ -19,13 +29,63 @@ public:
     game_object(const game_object &)            = delete;
     game_object &operator=(const game_object &) = delete;
 
-    virtual void on_init()                              = 0;
-    virtual void on_update(std::chrono::nanoseconds dt) = 0;
-    virtual void on_render(base::renderer &) const      = 0;
+    virtual void on_init() = 0;
+    virtual void on_update(std::chrono::nanoseconds dt) {};
+    virtual void on_render(base::renderer &) {};
 
     std::uint32_t id() const noexcept
     {
         return p_id;
+    }
+
+    struct transform &transform() noexcept
+    {
+        return p_tf;
+    }
+
+    const struct transform &transform() const noexcept
+    {
+        return p_tf;
+    }
+
+    type::world_position &position() noexcept
+    {
+        return p_tf.pos;
+    }
+
+    const type::world_position &position() const noexcept
+    {
+        return p_tf.pos;
+    }
+
+    type::world_size &size() noexcept
+    {
+        return p_tf.size;
+    }
+
+    const type::world_size &size() const noexcept
+    {
+        return p_tf.size;
+    }
+
+    float &rotation() noexcept
+    {
+        return p_tf.rotation;
+    }
+
+    const float &rotation() const noexcept
+    {
+        return p_tf.rotation;
+    }
+
+    type::vec2f &pivot() noexcept
+    {
+        return p_tf.pivot;
+    }
+
+    const type::vec2f &pivot() const noexcept
+    {
+        return p_tf.pivot;
     }
 
     bool is_active() const noexcept
@@ -49,13 +109,12 @@ public:
     }
 
 protected:
-    type::world_position position;
-    type::world_size size;
     bool active{true};
     bool should_remove{false};
 
 private:
     std::uint32_t p_id;
+    struct transform p_tf;
 };
 
 }; // namespace sgf
