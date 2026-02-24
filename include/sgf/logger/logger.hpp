d1156026fea9ba960efcf3f22d89eecb051745a0
TPPPP <906483498@qq.com>
2026-02-25 01:00:27 +0800

    feat(logger): remove windows.h in header to avoid global pollution

--- include/sgf/logger/logger.hpp
+++ include/sgf/logger/logger.hpp
@@ -1,18 +1,6 @@
 #ifndef SGF_LOGGER_HPP
 #define SGF_LOGGER_HPP
 
-#include <cstdarg>
-#include <cstdio>
-#include <string>
-
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
 namespace sgf
 {
 
@@ -26,42 +14,7 @@ enum class log_level
 class logger
 {
 public:
-    static void log(log_level level, const char *fmt, ...)
-    {
-        char buffer[1024];
-        va_list args;
-        va_start(args, fmt);
-        std::vsnprintf(buffer, sizeof(buffer), fmt, args);
-        va_end(args);
-
-        const char *color_code = "";
-        const char *reset_code = "\033[0m";
-        const char *prefix     = "";
-
-        switch (level)
-        {
-        case log_level::info:
-            color_code = "\033[32m";
-            prefix     = "[INFO] ";
-            break;
-        case log_level::warn:
-            color_code = "\033[33m";
-            prefix     = "[WARN] ";
-            break;
-        case log_level::error:
-            color_code = "\033[31m";
-            prefix     = "[ERROR] ";
-            break;
-        }
-
-        std::string final_msg = std::string(color_code) + prefix + buffer + reset_code + "\n";
-
-        std::printf("%s", final_msg.c_str());
-
-#ifdef _WIN32
-        OutputDebugStringA(final_msg.c_str());
-#endif
-    }
+    static void log(log_level level, const char *fmt, ...);
 };
 
 #define SGF_INFO(fmt, ...) sgf::logger::log(sgf::log_level::info, fmt, ##__VA_ARGS__)
---
+++ src/core/logger/logger.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <sgf/logger/logger.hpp>
+#include <string>
+
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#endif
+
+void sgf::logger::log(log_level level, const char *fmt, ...)
+{
+    char buffer[1024];
+    va_list args;
+    va_start(args, fmt);
+    std::vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
+
+    const char *color_code = "";
+    const char *reset_code = "\033[0m";
+    const char *prefix     = "";
+
+    switch (level)
+    {
+    case log_level::info:
+        color_code = "\033[32m";
+        prefix     = "[INFO] ";
+        break;
+    case log_level::warn:
+        color_code = "\033[33m";
+        prefix     = "[WARN] ";
+        break;
+    case log_level::error:
+        color_code = "\033[31m";
+        prefix     = "[ERROR] ";
+        break;
+    }
+
+    std::string final_msg = std::string(color_code) + prefix + buffer + reset_code + "\n";
+    std::printf("%s", final_msg.c_str());
+
+#ifdef _WIN32
+    OutputDebugStringA(final_msg.c_str());
+#endif
+}
